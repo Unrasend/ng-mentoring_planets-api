aafa8b093a4037db75047deaada59bd63a92f4ef
Vlad Luchkov <unrasend@gmail.com>
2023-02-10 15:37:11 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
+    '/planets/:id': '/planets/:id'
 }))
 server.use(router)
 server.listen(3000, () => {
